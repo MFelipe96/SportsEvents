3a40150384b9a21547f965560f711aec97b248d6
MFelipe96 <cc.ufscar@gmail.com>
2025-06-25 00:40:11 +0100

    Improve sorting to keep outdated events at the bottom of the list

--- app/src/main/java/com/mypackage/sportsevents/domain/usecase/GetSportsUseCase.kt
+++ app/src/main/java/com/mypackage/sportsevents/domain/usecase/GetSportsUseCase.kt
@@ -1,5 +1,6 @@
 package com.mypackage.sportsevents.domain.usecase
 
+import com.mypackage.sportsevents.domain.model.Event
 import com.mypackage.sportsevents.domain.model.Sport
 import com.mypackage.sportsevents.domain.repository.SportsRepository
 import javax.inject.Inject
@@ -9,8 +10,14 @@ class GetSportsUseCase @Inject constructor(
 ) {
     suspend operator fun invoke(): List<Sport> {
         val sports = repository.getAllSports()
+        val now = System.currentTimeMillis() / 1000
         return sports.map { sport ->
-            sport.copy(events = sport.events.sortedBy { it.timestamp })
+            val sortedEvents = sport.events.sortedWith(compareBy<Event> {
+                it.timestamp < now
+            }.thenBy {
+                it.timestamp
+            })
+            sport.copy(events = sortedEvents)
         }
     }
 }
