fa865509ebb9b029a3f4aba17f15683d0f1557fe
MFelipe96 <cc.ufscar@gmail.com>
2025-06-25 10:40:04 +0100

    Add mapper to convert between dto, entity and domain models

---
+++ app/src/main/java/com/mypackage/sportsevents/data/local/mapper/EventMapper.kt
@@ -0,0 +1,24 @@
+package com.mypackage.sportsevents.data.local.mapper
+
+import com.mypackage.sportsevents.data.local.entity.EventEntity
+import com.mypackage.sportsevents.data.remote.dto.EventDto
+import com.mypackage.sportsevents.domain.model.Event
+
+fun EventDto.toEntity(sportId: String, sportName: String): EventEntity {
+    return EventEntity(
+        id = this.id,
+        sportId = sportId,
+        sportName = sportName,
+        name = this.name,
+        timestamp = this.timestamp
+    )
+}
+
+fun EventEntity.toDomain(): Event {
+    return Event(
+        id = this.id,
+        name = this.name,
+        timestamp = this.timestamp,
+        isFavorite = this.isFavorite
+    )
+}
