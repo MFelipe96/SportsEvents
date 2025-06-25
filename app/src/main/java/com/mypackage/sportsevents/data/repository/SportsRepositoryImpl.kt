96b206b2b404d6e76b4c1e3e04234cbd5441b3a5
MFelipe96 <cc.ufscar@gmail.com>
2025-06-25 01:20:11 +0100

    Add error handling and network helper

--- app/src/main/java/com/mypackage/sportsevents/data/repository/SportsRepositoryImpl.kt
+++ app/src/main/java/com/mypackage/sportsevents/data/repository/SportsRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package com.mypackage.sportsevents.data.repository
 
+import android.accounts.NetworkErrorException
 import com.mypackage.sportsevents.data.remote.api.SportsApi
 import com.mypackage.sportsevents.data.remote.mapper.toDomain
 import com.mypackage.sportsevents.domain.model.Sport
 import com.mypackage.sportsevents.domain.repository.SportsRepository
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class SportsRepositoryImpl @Inject constructor(
@@ -11,5 +13,13 @@ class SportsRepositoryImpl @Inject constructor(
 ) : SportsRepository {
     override suspend fun getAllSports(): List<Sport> {
         return api.getSports().map { it.toDomain() }
+        try {
+            val response = api.getSports()
+            return response.map { it.toDomain() }
+        } catch (e: HttpException) {
+            throw NetworkErrorException("Server error: ${e.code()}")
+        }catch (e: Exception) {
+            throw e
+        }
     }
 }
--- app/src/main/java/com/mypackage/sportsevents/domain/usecase/GetSportsUseCase.kt
+++ app/src/main/java/com/mypackage/sportsevents/domain/usecase/GetSportsUseCase.kt
@@ -3,12 +3,18 @@ package com.mypackage.sportsevents.domain.usecase
 import com.mypackage.sportsevents.domain.model.Event
 import com.mypackage.sportsevents.domain.model.Sport
 import com.mypackage.sportsevents.domain.repository.SportsRepository
+import com.mypackage.sportsevents.utils.NetworkHelper
 import javax.inject.Inject
 
 class GetSportsUseCase @Inject constructor(
-    private val repository: SportsRepository
+    private val repository: SportsRepository,
+    private val networkHelper: NetworkHelper
 ) {
     suspend operator fun invoke(): List<Sport> {
+        if (!networkHelper.isNetworkAvailable()) {
+            error("No internet connection")
+        }
+
         val sports = repository.getAllSports()
         val now = System.currentTimeMillis() / 1000
         return sports.map { sport ->
